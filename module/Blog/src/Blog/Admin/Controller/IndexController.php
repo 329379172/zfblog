3faf122a53e139f401938f53e56ce24cdf1935df
329379172@qq.com <xiaoqiu>
2015-06-20 11:45:38 +0800

    controller

--- module/Blog/src/Blog/Admin/Controller/IndexController.php
+++ module/Blog/src/Blog/Admin/Controller/IndexController.php
@@ -5,10 +5,8 @@
  * Date: 15/6/15
  * Time: 下午2:54
  */
-namespace Blog\Controller;
+namespace Blog\Controller\Admin;
 
-use Blog\Model\Article;
-use Zend\Debug\Debug;
 use Zend\Mvc\Controller\AbstractActionController;
 
 class IndexController extends AbstractActionController{
