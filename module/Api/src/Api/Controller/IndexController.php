288be5fecc95b8d5c42855ff5d0a8b2ebe551b9d
329379172@qq.com <xiaoqiu>
2015-07-06 22:18:53 +0800

    邮编查询更加详细

--- module/Api/src/Api/Controller/IndexController.php
+++ module/Api/src/Api/Controller/IndexController.php
@@ -11,13 +11,18 @@ use Zend\Cache\Storage\Adapter\Redis;
 use Zend\Cache\Storage\Adapter\RedisOptions;
 use Zend\Mvc\Controller\AbstractActionController;
 
-class IndexController extends AbstractActionController{
+ini_set("display_errors", "On");
+error_reporting(E_ALL | E_STRICT);
 
-    function getTaobaoUserInfoAction(){
+class IndexController extends AbstractActionController
+{
+
+    function getTaobaoUserInfoAction()
+    {
         $nick = urlencode($this->params('name'));
         $redis = $this->getServiceLocator()->get('Redis');
         //$redis = new Redis();
-        if($redis->getItem(md5($nick))){
+        if ($redis->getItem(md5($nick))) {
             echo $redis->getItem(md5($nick));
             exit;
         }
@@ -26,32 +31,32 @@ class IndexController extends AbstractActionController{
             'Referer' => 'http://www.131458.com/',
             'Content-Type' => 'application/json; charset=UTF-8'
         ];
-        $data = "{'nick':'"  . $nick . "'}";
-        $response = \Requests::post('http://www.131458.com/handler/load.aspx/Load',$headers,$data);
-        $d = json_decode($response->body,true);
+        $data = "{'nick':'" . $nick . "'}";
+        $response = \Requests::post('http://www.131458.com/handler/load.aspx/Load', $headers, $data);
+        $d = json_decode($response->body, true);
         $session = $response->cookies['ASP.NET_SessionId']->value;
-        if(is_array($d) && !empty($d['d'])){
-            $tokenResponse = \Requests::get('http://localhost:3000/token?d='.$d['d'].'&c=vvl');
+        if (is_array($d) && !empty($d['d'])) {
+            $tokenResponse = \Requests::get('http://localhost:3000/token?d=' . $d['d'] . '&c=vvl');
             $headers = [
-                'Cookie' => 'ASP.NET_SessionId='.$session
+                'Cookie' => 'ASP.NET_SessionId=' . $session
             ];
-            $url = 'http://www.131458.com/handler/TaobaoInfo.ashx?tbNickInfoJson='  . $nick .  '&token=' . $tokenResponse->body . '&_=' . (time() * 1000);
-            $taobaoInfoResponse = \Requests::get($url,$headers);
-            $url = 'http://www.131458.com/handler/TaobaoInfo.ashx?nickCode='  . $nick .  '&token=' . $tokenResponse->body . '&_=' . (time() * 1000);
-            $taobaoInfoResponse2 = \Requests::get($url,$headers);
+            $url = 'http://www.131458.com/handler/TaobaoInfo.ashx?tbNickInfoJson=' . $nick . '&token=' . $tokenResponse->body . '&_=' . (time() * 1000);
+            $taobaoInfoResponse = \Requests::get($url, $headers);
+            $url = 'http://www.131458.com/handler/TaobaoInfo.ashx?nickCode=' . $nick . '&token=' . $tokenResponse->body . '&_=' . (time() * 1000);
+            $taobaoInfoResponse2 = \Requests::get($url, $headers);
 
             $document = str_get_html($taobaoInfoResponse2->body);
             $info = [];
             $info['nickName'] = $document->find('.inq_02_L_001')[0]->find('a')[0]->text();
-            $info['registerTime'] =  $document->find('.inq_02_L_002')[0]->find('span')[0]->text();
-            $info['realName'] =  $document->find('.inq_02_L_003')[0]->find('span')[0]->text();
-            $info['address'] =  $document->find('.inq_02_L_004')[1]->find('span')[0]->text();
+            $info['registerTime'] = $document->find('.inq_02_L_002')[0]->find('span')[0]->text();
+            $info['realName'] = $document->find('.inq_02_L_003')[0]->find('span')[0]->text();
+            $info['address'] = $document->find('.inq_02_L_004')[1]->find('span')[0]->text();
             $info['zhongping'] = $document->find('#zhongpingNumber')[0]->text();
             $info['chapingNumber'] = $document->find('#chapingNumber')[0]->text();
             $info['bili'] = $document->find('#zcbl')[0]->text();
             $info['buyerLevel'] = $document->find('.inq_04_001')[0]->find('img')[0]->attr['src'];
             $dom = $document->find('.inq_04_001')[0]->find('a');
-            if(count($dom) > 0){
+            if (count($dom) > 0) {
                 $info['buyerVipLevel'] = $document->find('.inq_04_001')[0]->find('a')[0]->find('img')[0]->attr['title'];
             }
             $info['buyerCredit'] = $document->find('.inq_04_001')[0]->find('b[name=p]')[0]->text();
@@ -67,32 +72,39 @@ class IndexController extends AbstractActionController{
             $info['sellerMonth'] = $document->find('.inq_04_001')[8]->find('b')[0]->text();
             $info['sellerHalfYear'] = $document->find('.inq_04_001')[9]->find('b')[0]->text();
             $info['sellerHalfYearAgo'] = $document->find('.inq_04_001')[10]->find('b')[0]->text();
-            $infojson = json_decode($taobaoInfoResponse->body,true);
+            $infojson = json_decode($taobaoInfoResponse->body, true);
             $info['SecurityLevel'] = str_get_html(urldecode($infojson[0]['SecurityLevel']))->find('img')[0]->attr['src'];
-            $ret  = json_encode($info);
+            $ret = json_encode($info);
             echo $ret;
-            $redis->setItem(md5($nick),$ret);
-        }else{
+            $redis->setItem(md5($nick), $ret);
+        } else {
             echo '';
         }
         exit;
     }
 
-    public function getPostCodeAction(){
-        $query = urlencode(iconv('utf-8','gbk',$this->params('name')));
+    public function getPostCodeAction()
+    {
+        $query = urlencode(iconv('utf-8', 'gbk', $this->params('name')));
         $response = \Requests::get('http://opendata.baidu.com/post/s?wd=' . $query . '&rn=20');
         $document = str_get_html($response->body);
         $listData = $document->find('.list-data ul li a');
-        if(count($listData) > 0){
-            $arr = explode(iconv('utf-8','gbk',' '),$listData[0]->text());
-            $ret = $arr[count($arr) - 1];
-            echo $ret;
+        if (count($listData) > 0) {
+            $text = $listData[0]->text();
+            $arr = explode(iconv('utf-8', 'gbk', ' '), $text);
+            $index = $arr[count($arr) - 1];
+            echo json_encode([$index=> iconv('gbk', 'utf-8',$arr[1])],JSON_UNESCAPED_UNICODE);
             exit;
         }
 
         $tableData = $document->find('.table-data .table-list tbody tr');
-        if(count($tableData) > 1){
-            echo $tableData[1]->find('td')[0]->text();
+        $retArr = [];
+        if (count($tableData) > 1) {
+            for ($i = 1; $i < count($tableData); $i++) {
+                $index = $tableData[$i]->find('td')[0]->text();
+                $retArr[$index] = iconv('gbk', 'utf-8', $tableData[$i]->find('td')[1]->text());
+            }
+            echo json_encode($retArr,JSON_UNESCAPED_UNICODE);
             exit;
         }
         exit;
