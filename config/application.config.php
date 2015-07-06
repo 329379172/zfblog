0dfc92ceb4bba8a214d9f4476aaa3d9266d71e24
卢鹏飞 <329379172@qq.com>
2015-07-06 14:23:32 +0800

    加载admin模块

--- config/application.config.php
+++ config/application.config.php
@@ -10,7 +10,8 @@ return array(
     'modules' => array(
         'Redis',
         'Application',
-        'Api'
+        'Api',
+        'Admin'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
